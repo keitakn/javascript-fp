0b81266c8b302b8f203ea7c822891126807c9ecf
keitakn <keita.koga.work@gmail.com>
2018-07-11 02:25:17 +0900

    :sparkles: #4 SQLっぽい検索用の関数を実施

--- src/chapter3/Chapter3.ts
+++ src/chapter3/Chapter3.ts
@@ -1,5 +1,14 @@
+import * as _ from "lodash";
 import { People } from "../domain/types/People";
 import { Person } from "../domain/types/Person";
+
+/**
+ * sqlLikeSearch Response IF
+ */
+interface NameColumn {
+  givenName: string;
+  familyName: string;
+}
 
 /**
  * 第3章 データ構造の数を減らし、操作の数を増やす
@@ -19,5 +28,52 @@ export namespace Chapter3 {
 
       return stat;
     }, {});
+  };
+
+  /**
+   * 日本人かどうかを判定する
+   * SQLで言うところのWHERE
+   * この関数相当の物を入れ替える事によって sqlLikeSearch() は汎用的な関数になる
+   *
+   * @param {Person} person
+   * @returns {boolean}
+   */
+  const isJapanesePeople = (person: Person) => {
+    return person.address.country === "JP";
+  };
+
+  /**
+   * Personから必要なColumnを選択する
+   * SQLで言うところのSELECT
+   * この関数相当の物を入れ替える事によって sqlLikeSearch() は汎用的な関数になる
+   *
+   * @param {Person} person
+   * @returns {NameColumn}
+   */
+  const selectNameColumn = (person: Person): NameColumn => {
+    return {
+      givenName: person.givenName,
+      familyName: person.familyName
+    };
+  };
+
+  /**
+   * SQLライクな検索関数
+   *
+   * map() = SELECT
+   * filter() = WHERE
+   * sortBy() = ORDER BY
+   *
+   * これらに渡す関数は自由に入れ替える事が出来るので汎用的であると言える
+   *
+   * @param {People} people
+   * @returns {NameColumn[]}
+   */
+  export const sqlLikeSearch = (people: People): NameColumn[] => {
+    return _.chain(people)
+      .filter((person: Person) => isJapanesePeople(person))
+      .sortBy(people, ["givenName"])
+      .map((person: Person) => selectNameColumn(person))
+      .value();
   };
 }
